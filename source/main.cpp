98fd5957c0be433f6c6aab87c260cf416f045c42
kC0d3 <kC0d3@github.com>
2024-11-22 10:17:13 +0100

    feat: Add SatelliteSniffer class and methods

--- source/main.cpp
+++ source/main.cpp
@@ -1,24 +1,9 @@
-#include <UI.h>
-#include <TLEtoGPS.h>
-#include <Utils.h>
-#include <PythonScriptExecutor.h>
+#include <SatelliteSniffer.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    PythonScriptExecutor::executeGetTLEDataPythonScript(getenv("USERNAME"), getenv("PASSWORD"));
-    //auto gps = TLEtoGPS::convertTLEToGPS("iss_last_tle.txt");
-    auto gps2 = TLEtoGPS::convertTLEToGPSAtTimeWindow("iss_last_tle.txt");
-    auto gps3 = TLEtoGPS::convertTLEToGPSAtTimeWindow("iss_last_tle.txt", 1000);
-    auto gps4 = TLEtoGPS::convertTLEToGPSAtTimeWindow("iss_last_tle.txt", 10000);
-
-    //Utils::printGPS(gps);
-    Utils::printGPS(gps2);
-    Utils::printGPS(gps3);
-    Utils::printGPS(gps4);
-
-    UI ui;
-    ui.run();
-
+    auto satelliteSniffer = SatelliteSniffer("Satellites.txt", 1024, 512);
+    satelliteSniffer.run();
     return 0;
 }
