4bf022b8d90acf9794e2b3cf211fdf3eb6018d83
kC0d3 <kC0d3@github.com>
2024-11-22 10:08:28 +0100

    feat: Add fillSatellitesBySourceFile method

--- include/SatelliteSniffer/Utils.h
+++ include/SatelliteSniffer/Utils.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <GPS.h>
 #include <chrono>
+#include <string>
+#include <vector>
+#include <Satellite.h>
 
 inline const auto current = std::chrono::system_clock::now();
 
@@ -12,4 +15,6 @@ namespace Utils {
     void printGPS(const GPS &gps);
 
     tm *getLocalTime(const int &milliseconds = 0);
+
+    void fillSatellitesBySourceFile(const std::string &filename, std::vector<Satellite> &satellites);
 }
