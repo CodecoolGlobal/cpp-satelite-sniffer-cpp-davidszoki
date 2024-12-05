12cfe77dd93d21716d4d48001227d13ad283c193
kC0d3 <kC0d3@github.com>
2024-12-05 11:40:28 +0100

    refactor: Move fetch method to constructor and add environment check

--- source/SatelliteSniffer/SatelliteSniffer.cpp
+++ source/SatelliteSniffer/SatelliteSniffer.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 SatelliteSniffer::SatelliteSniffer(const std::string &source, const int &width, const int &height)
     : ui(width, height, *this), source(source) {
+    if (getenv("USERNAME") && getenv("PASSWORD"))
+        PythonScriptExecutor::executeGetTLEDataPythonScript(getenv("USERNAME"), getenv("PASSWORD"), source);
     satellites = createSatellites();
 }
 
 void SatelliteSniffer::run(const int &speed) {
-    PythonScriptExecutor::executeGetTLEDataPythonScript(getenv("USERNAME"), getenv("PASSWORD"), source);
     ui.run(speed);
 }
 
@@ -20,7 +21,7 @@ std::vector<Satellite> SatelliteSniffer::createSatellites() const {
     return satellites;
 }
 
-void SatelliteSniffer::updatePositions(const int &width, const int &height, const int &updateTime){
+void SatelliteSniffer::updatePositions(const int &width, const int &height, const int &updateTime) {
     for (auto &sat: satellites) {
         sat.calculatePosition(sat, width, height, updateTime);
     }
